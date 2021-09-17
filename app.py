365ed74c5553f35f84c70bdad284f24b441285a3
Stefan Kochev <stefan_kochev@yahoo.com>
2021-09-17 19:59:58 +0200

    Add black formatter check

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import jsonify
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def endpoint():
     return jsonify({"message": "Test Service"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
